ecf4a922bee6f224715a4b88a501da794ea10c50
abdullah-621 <abdullahmasum621@gmail.com>
2025-12-03 06:47:24 +0600

    Get Method

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def aiquest():
   return {"Django": "API"}
+
+@app.get("/course")
+def course():
+  return {"course":"Machine Learning"}
+
+@app.get("/course/python")
+def python():
+  return {"python":"ML with Python", "Instractor":"Abdullah Al Masum", "Duration":"3 Months"}
