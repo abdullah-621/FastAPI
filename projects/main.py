ed9c348f4373fab3f389b0f3adc02fdea8c1094d
abdullah-621 <abdullahmasum621@gmail.com>
2026-02-11 13:04:05 +0600

    ADD status code and path parameter

--- projects/main.py
+++ projects/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -19,3 +19,13 @@ def about():
 @app.get("/views")
 def views():
   return load_data()
+
+
+@app.get("/patient/{patient_id}")
+def patient(patient_id : str = Path(..., description="ID of the patient", example="P001")):
+
+  data = load_data()
+
+  if patient_id in data:
+    return data[patient_id]
+  raise HTTPException(status_code=404, detail="Patient not found.")
